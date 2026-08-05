bd71600938fdd7427978f98bd92419eb7e5c99c3
sanhitamahapure24-boop <sanhitamahapure24@gmail.com>
2026-08-05 16:34:31 +0530

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,14 @@
+name = "Rahul"
+age = 20
+city = "Pune"
+dob = "20-01-2000"
+college = "TKA"
+blood_group = "A+"
+
+print("----- My ID Card -----")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("DOB:", dob)
+print("College:", college)
+print("Blood Group:", blood_group)
